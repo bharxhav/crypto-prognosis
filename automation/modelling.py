807522e1a5a6e0807a38c125ba77802d29e46e73
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2024-04-21 17:00:56 -0400

    Fix replacing mechanism

--- automation/modelling.py
+++ automation/modelling.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import re
 
 # Add the parent directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -15,6 +16,6 @@ with open('../README.md', 'r') as f:
 
 with open('../README.md', 'w') as f:
     for line in lines:
-        if 'Expected close for GBTC: ``' in line:
-            line = line.replace('Expected close for GBTC: ``', f'Expected close for GBTC today: `{prediction}`')
+        if 'Expected close' in line:
+            line = re.sub('(\d+(\.\d+)?)', f'{prediction}', line)
         f.write(line)
