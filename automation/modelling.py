5dd0b79d956141ecf61771455067cc866d627a3d
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2024-05-13 09:47:15 -0400

    Fix path

--- automation/modelling.py
+++ automation/modelling.py
@@ -4,8 +4,9 @@ import re
 
 # Add the parent directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../modules')))
 
-from token_predictor import TokenPredictor
+from modules.token_predictor import TokenPredictor
 
 tp = TokenPredictor()
 tp.train(directory="../data/")
